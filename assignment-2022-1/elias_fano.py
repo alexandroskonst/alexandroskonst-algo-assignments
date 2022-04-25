91e15e9471e01c183bbc9c59118b652b279c3820
alexandroskonst <alexandros_konst@outlook.com>
2022-04-25 18:32:11 +0300

    added 3 more fuctions for the construction of the L bytearray. One of them is still under construction (what to append in List.)

--- assignment-2022-1/elias_fano.py
+++ assignment-2022-1/elias_fano.py
@@ -9,14 +9,64 @@ def get_last_digits_from_binary(value, num_of_dig):
 def get_first_digits_from_binary(value, num_of_dig):
     bits = value >> num_of_dig
     return bits
+     
+def add_bits(byte, bits, bits_num):
+    shift = byte << bits_num
+    nbyte = shift | bits
+    return nbyte
 
-def bit_combo(value1, value2, l, lenb):
-    if lenb + l <= 8:
-        fvalue = (value1 << l) | value2
+def cut_bits(bits, num_to_remove, to_add):
+    bits_to_remove = get_last_digits_from_binary(bits, num_to_remove)
+    nbits = get_first_digits_from_binary(bits, num_to_remove)
+    return bin(nbits)[2:].zfill(to_add), bin(bits_to_remove)[2:].zfill(num_to_remove)
+
+def bit_combo(byte, bits, bits_num, lenb):
+    if lenb + bits_num <= 8:
+        nbyte = add_bits(byte, bits, bits_num)
+        nlenb = lenb + bits_num
+        return nbyte, nlenb, 0, 0
     else:
-        rem_bits = l - 8 - lenb
-        
+        bits_to_add = 8 - lenb
+        bits_to_remove = bits_num - bits_to_add
+        cut_bits_ = cut_bits(bits, bits_to_remove, bits_to_add)
+        nbits = int(cut_bits_[0], 2)
+        bits_removed = cut_bits_[1]
+        nbyte = add_bits(byte, nbits, bits_to_add)
+        return nbyte, 8, bits_to_remove, bits_removed
 
+def create_byte(list, l):
+    init_byte = bit_combo(int(list[0], 2), int(list[1], 2), l, l)
+    byte = init_byte
+    th = 1
+    while byte[1] < 8 and th < len(list) - 1:
+        th += 1
+        k = bin(byte[0])[2:].zfill(byte[1])
+        byte = bit_combo(int(k, 2), int(list[th], 2), l, byte[1])
+    return byte
+
+def bitlist_to_bytes(list, l): #not ready yet, under construction 
+    elements_to_add = math.ceil(8 / l) 
+    print(elements_to_add)
+    bytelist = []
+    bitlist = list[0:elements_to_add]
+    print(bitlist)
+    while not parsed:
+        cr_byte = create_byte(bitlist, l)
+        byte = bin(cr_byte[0])[2:].zfill(8)
+        bytelist.append(byte)
+        print(bytelist)
+        if cr_byte[2] > 0: 
+            print ('xxx')
+            print(cr_byte)
+            bits_left = cr_byte[3].zfill(cr_byte[2])
+            print('bits left')
+            print('xxxx')
+            print(bits_left)
+            bitlist = []
+            bitlist.append(bits_left)
+            #bitlist.append(????) 
+        parsed = True
+    return bitlist
 #get_first_digits_from_binary(0b0010011101, 2)
 sorted_list = [5, 8, 11, 20, 33]
 
